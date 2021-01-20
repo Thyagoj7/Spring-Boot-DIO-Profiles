1231b3744c116fe99fac4c79d9b86bf734e490e7
Thyago <t.y_josef7@hotmail.com>
2021-01-20 15:49:35 -0300

    commit

--- springbootconfig/src/main/java/com/digitalinnovationonepart2/springbootconfig/config/AppController.java
+++ springbootconfig/src/main/java/com/digitalinnovationonepart2/springbootconfig/config/AppController.java
@@ -10,10 +10,18 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NENHUMA}")
+    public String dbEnvironmentVariable;
+
 
     @GetMapping("/")
     public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getEnvironmentVariable() {
+        return "A seginte variavel de ambiente foi passada" + dbEnvironmentVariable;
     }
 }
 
